d52bb29a63561dabf14280f69e304415481cf889
ChenYuanFang <yuanfang.chen@bys053.local>
2013-04-27 13:41:33 +0800

    Initial Commit

---
+++ MobileMall/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  MobileMall
+//
+//  Created by ChenYuanFang on 13-4-27.
+//  Copyright (c) 2013年 ChenYuanFang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
